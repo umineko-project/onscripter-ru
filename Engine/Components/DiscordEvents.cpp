09818490a67341c6d18d28175f9e17f17339e491
metafive <m3t4f1v3@users.noreply.github.com>
2022-07-10 11:27:15 +1000

    actually fix clang formatting

--- Engine/Components/DiscordEvents.cpp
+++ Engine/Components/DiscordEvents.cpp
@@ -3,51 +3,51 @@
 #include "Support/FileIO.hpp"
 #include <iostream>
 struct DiscordState {
-    discord::User currentUser;
+	discord::User currentUser;
 
-    std::unique_ptr<discord::Core> core;
+	std::unique_ptr<discord::Core> core;
 };
 
 DiscordState state{};
 discord::Core* core{};
 
 void initDiscord(const char* id) {
-  auto result = discord::Core::Create(strtoll(id, NULL, 10), DiscordCreateFlags_NoRequireDiscord, &core);
-  state.core.reset(core);
-  if (!state.core) {
-      sendToLog(LogLevel::Error, "%d\n", static_cast<int>(result));
-      std::exit(-1);
-  }
-  state.core->SetLogHook(
-    discord::LogLevel::Debug, [](discord::LogLevel level, const char* message) {
-        sendToLog(LogLevel::Error, "Log(%d) %s\n", static_cast<uint32_t>(level), message);
-    });
+	auto result = discord::Core::Create(strtoll(id, NULL, 10), DiscordCreateFlags_NoRequireDiscord, &core);
+	state.core.reset(core);
+	if (!state.core) {
+		sendToLog(LogLevel::Error, "%d\n", static_cast<int>(result));
+		std::exit(-1);
+	}
+	state.core->SetLogHook(
+	    discord::LogLevel::Debug, [](discord::LogLevel level, const char* message) {
+		    sendToLog(LogLevel::Error, "Log(%d) %s\n", static_cast<uint32_t>(level), message);
+	    });
 }
 
-void setPresence(const char* details, const char* currentState, const char* largeImageKey, const char* largeImageText, const char* smallImageKey, const char* smallImageText, const char* startTimestamp, const char* endTimestamp=NULL) {
-  discord::Activity activity{};
-  activity.SetDetails(details);
-  activity.SetState(currentState);
-  activity.GetAssets().SetSmallImage(smallImageKey);
-  activity.GetAssets().SetSmallText(smallImageText);
-  activity.GetAssets().SetLargeImage(largeImageKey);
-  activity.GetAssets().SetLargeText(largeImageText);
-  activity.GetTimestamps().SetStart(strtoll(startTimestamp, NULL, 10));
-  activity.GetTimestamps().SetEnd(strtoll(endTimestamp, NULL, 10));
-  activity.SetType(discord::ActivityType::Playing);
-  state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {
-      sendToLog(((result == discord::Result::Ok) ? LogLevel::Info : LogLevel::Error), "Updating activity!\n");
-  });
+void setPresence(const char* details, const char* currentState, const char* largeImageKey, const char* largeImageText, const char* smallImageKey, const char* smallImageText, const char* startTimestamp, const char* endTimestamp = NULL) {
+	discord::Activity activity{};
+	activity.SetDetails(details);
+	activity.SetState(currentState);
+	activity.GetAssets().SetSmallImage(smallImageKey);
+	activity.GetAssets().SetSmallText(smallImageText);
+	activity.GetAssets().SetLargeImage(largeImageKey);
+	activity.GetAssets().SetLargeText(largeImageText);
+	activity.GetTimestamps().SetStart(strtoll(startTimestamp, NULL, 10));
+	activity.GetTimestamps().SetEnd(strtoll(endTimestamp, NULL, 10));
+	activity.SetType(discord::ActivityType::Playing);
+	state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {
+		sendToLog(((result == discord::Result::Ok) ? LogLevel::Info : LogLevel::Error), "Updating activity!\n");
+	});
 }
 
 void runDiscordCallbacks() {
-  state.core->RunCallbacks();
+	state.core->RunCallbacks();
 }
 
-void shutdownDiscord(){
-  state.core->ActivityManager().ClearActivity([](discord::Result result) {
-      sendToLog(((result == discord::Result::Ok) ? LogLevel::Info : LogLevel::Error), "Stopping discord!\n");
-  });
+void shutdownDiscord() {
+	state.core->ActivityManager().ClearActivity([](discord::Result result) {
+		sendToLog(((result == discord::Result::Ok) ? LogLevel::Info : LogLevel::Error), "Stopping discord!\n");
+	});
 }
 
 #endif
--- Engine/Components/DiscordEvents.hpp
+++ Engine/Components/DiscordEvents.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 void initDiscord(const char* id);
-void setPresence(const char* details, const char* currentState, const char* largeImageKey, const char* largeImageText, const char* smallImageKey, const char* smallImageText, const char* startTimestamp, const char* endTimestamp=NULL);
+void setPresence(const char* details, const char* currentState, const char* largeImageKey, const char* largeImageText, const char* smallImageKey, const char* smallImageText, const char* startTimestamp, const char* endTimestamp = NULL);
 void runDiscordCallbacks();
 void shutdownDiscord();
