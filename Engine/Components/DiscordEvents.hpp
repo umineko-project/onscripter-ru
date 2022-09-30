596f9a972af38f6c6034521c382082d0b1b63024
m3t4f1v3 <m3t4f1v3@users.noreply.github.com>
2022-09-30 22:36:12 +1000

    slightly try to fix discord and rename the release

--- Engine/Components/DiscordEvents.hpp
+++ Engine/Components/DiscordEvents.hpp
@@ -8,8 +8,12 @@
  */
 
 #pragma once
+#include <discord/discord.h>
+#include "Support/FileIO.hpp"
+
 
 void initDiscord(const char* id);
 void setPresence(const char* details, const char* currentState, const char* largeImageKey, const char* largeImageText, const char* smallImageKey, const char* smallImageText, const char* startTimestamp, const char* endTimestamp = NULL);
 void runDiscordCallbacks();
 void shutdownDiscord();
+LogLevel translateLogLevel(discord::LogLevel level);
